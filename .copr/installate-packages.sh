785edd572c9db1b138862414f24884d468a058dc
David Cantrell <dcantrell@redhat.com>
2019-04-08 13:43:08 -0400

    Install the spec file BuildRequires for copr builds.

--- .copr/installate-packages.sh
+++ .copr/installate-packages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 PATH=/usr/bin
+TOPDIR="$1"
 
 # None of us is as dumb as all of us.
 yum --help >/dev/null 2>&1
@@ -8,3 +9,6 @@ yum --help >/dev/null 2>&1
 
 # Things required to build the source directly from git
 ${INSTALLATOR} install -y automake autoconf libtool
+
+# Install the BuildRequires from the spec file
+${INSTALLATOR} install -y $(grep BuildRequires: "${TOPDIR}/rpminspect.spec.in" | awk '{ print $2; }')
