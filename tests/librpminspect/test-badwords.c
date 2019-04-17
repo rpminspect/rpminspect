e317a6bbd38f8471c1da3bba854a2a154928f36c
David Cantrell <dcantrell@redhat.com>
2019-04-17 13:14:39 -0400

    Begin unit test suite using CUnit.
    These tests are going to use CUnit to perform unit tests where
    applicable in the source code.  More information here:
    
        http://cunit.sourceforge.net/
    
    If you lack CUnit, the unit tests are disabled.  The idea here being we
    will still perform integration tests.

---
+++ tests/librpminspect/test-badwords.c
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2019  Red Hat, Inc.
+ * Author(s):  David Cantrell <dcantrell@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <CUnit/Basic.h>
+#include "rpminspect.h"
+
+string_list_t *forbidden_words = NULL;
+
+int init_test_badwords(void) {
+    string_entry_t *entry;
+
+    /* create a list of forbidden words */
+    if ((forbidden_words = malloc(sizeof(*forbidden_words))) == NULL) {
+        return -1;
+    }
+
+    TAILQ_INIT(forbidden_words);
+
+    /* add test words */
+    if ((entry = calloc(1, sizeof(*entry))) == NULL) {
+        return -1;
+    }
+
+    entry->data = strdup("foo");
+    TAILQ_INSERT_TAIL(forbidden_words, entry, items);
+
+    if ((entry = calloc(1, sizeof(*entry))) == NULL) {
+        return -1;
+    }
+
+    entry->data = strdup("bar");
+    TAILQ_INSERT_TAIL(forbidden_words, entry, items);
+
+    if ((entry = calloc(1, sizeof(*entry))) == NULL) {
+        return -1;
+    }
+
+    entry->data = strdup("baz");
+    TAILQ_INSERT_TAIL(forbidden_words, entry, items);
+
+    if ((entry = calloc(1, sizeof(*entry))) == NULL) {
+        return -1;
+    }
+
+    entry->data = strdup("qux");
+    TAILQ_INSERT_TAIL(forbidden_words, entry, items);
+
+    return 0;
+}
+
+int clean_test_badwords(void) {
+    list_free(forbidden_words, free);
+    return 0;
+}
+
+void test_has_bad_word(void) {
+    CU_ASSERT(has_bad_word("foo", forbidden_words) == true);
+    CU_ASSERT(has_bad_word("bar", forbidden_words) == true);
+    CU_ASSERT(has_bad_word("baz", forbidden_words) == true);
+    CU_ASSERT(has_bad_word("qux", forbidden_words) == true);
+    CU_ASSERT(has_bad_word("flargenblarfle", forbidden_words) == false);
+    CU_ASSERT(has_bad_word("cocacola", forbidden_words) == false);
+    CU_ASSERT(has_bad_word("suse", forbidden_words) == false);
+    CU_ASSERT(has_bad_word("supermonkeyball", forbidden_words) == false);
+}
+
+int main(void) {
+    CU_pSuite pSuite = NULL;
+
+    /* initialize this test registry */
+    if (CU_initialize_registry() != CUE_SUCCESS) {
+        return CU_get_error();
+    }
+
+    /* add a suite to the registry */
+    pSuite = CU_add_suite("badwords", init_test_badwords, clean_test_badwords);
+    if (pSuite == NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* add tests to the suite */
+    if (CU_add_test(pSuite, "test has_bad_word()", test_has_bad_word) == NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* run all tests using the CUnit basic interface */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
