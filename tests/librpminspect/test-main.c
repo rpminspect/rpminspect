a92e47f43928415fc736b0cf3c79e71a7609e113
David Shea <dshea@redhat.com>
2019-04-24 13:49:41 -0400

    Use config.h in the test files.

--- tests/librpminspect/test-main.c
+++ tests/librpminspect/test-main.c
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
+
 #include <stdlib.h>
 
 #include <CUnit/Basic.h>
--- tests/librpminspect/test-tty.c
+++ tests/librpminspect/test-tty.c
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
+
 #include <unistd.h>
 #include <CUnit/Basic.h>
 #include "rpminspect.h"
