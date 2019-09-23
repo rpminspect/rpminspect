c4a54f099bc938d334d5e83960ebe10157383e96
David Cantrell <dcantrell@redhat.com>
2019-09-23 15:11:09 -0400

    Move more common test stuff to baseclass.py
    Create two new classes:  TestSRPM and TestRPMs
    
    The TestSRPM class sets up a test case that only runs against the SRPM
    of a build.  The TestRPMs class sets up a test case that runs only
    against the binary RPMs from a build.
    
    Individual test cases can inherit from these two classes and just extend
    the setUp() method.  The runTest() method should be generic enough for
    most things.

--- tests/baseclass.py
+++ tests/baseclass.py
@@ -17,7 +17,10 @@
 #
 
 import os
+import subprocess
 import unittest
+import json
+import rpmfluff
 
 # Exceptions used by the test suite
 class MissingRpminspect(Exception):
@@ -38,3 +41,77 @@ class RequiresRpminspect(unittest.TestCase):
 
         if not os.path.isfile(self.conffile) or not os.access(self.conffile, os.R_OK):
             raise MissingRpminspectConf
+
+# Base test case class that tests on the SRPM package only
+class TestSRPM(RequiresRpminspect):
+    def setUp(self):
+        RequiresRpminspect.setUp(self)
+        self.rpm = rpmfluff.SimpleRpmBuild("example", "0.1", "1")
+
+        # the inheriting class needs to override these
+        self.inspection = None
+        self.label = None
+
+        # these default to 0 and OK
+        self.exitcode = 0
+        self.result = 'OK'
+
+    def runTest(self):
+        if not self.inspection:
+            return
+
+        self.rpm.make()
+        self.p = subprocess.Popen([self.rpminspect,
+                                   '-c', self.conffile,
+                                   '-F', 'json',
+                                   '-T', self.inspection,
+                                   self.rpm.get_built_srpm()],
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+        (self.out, self.err) = self.p.communicate()
+        self.results = json.loads(self.out)
+
+        # anything not OK or INFO is a non-zero return
+        if self.result not in ['OK', 'INFO'] and self.exitcode == 0:
+            self.exitcode = 1
+
+        self.assertEqual(self.p.returncode, self.exitcode)
+        self.assertEqual(self.results[self.label][0]['result'], self.result)
+
+# Base test case class that tests the binary RPMs
+class TestRPMs(RequiresRpminspect):
+    def setUp(self):
+        RequiresRpminspect.setUp(self)
+        self.rpm = rpmfluff.SimpleRpmBuild("example", "0.1", "1")
+
+        # the inheriting class needs to override these
+        self.inspection = None
+        self.label = None
+
+        # these default to 0 and OK
+        self.exitcode = 0
+        self.result = 'OK'
+
+    def runTest(self):
+        if not self.inspection and not self.label:
+            return
+
+        self.rpm.make()
+
+        # anything not OK or INFO is a non-zero return
+        if self.result not in ['OK', 'INFO'] and self.exitcode == 0:
+            self.exitcode = 1
+
+        for a in self.rpm.get_build_archs():
+            self.p = subprocess.Popen([self.rpminspect,
+                                       '-c', self.conffile,
+                                       '-F', 'json',
+                                       '-T', self.inspection,
+                                       self.rpm.get_built_rpm(a)],
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+            (self.out, self.err) = self.p.communicate()
+            self.results = json.loads(self.out)
+
+            self.assertEqual(self.p.returncode, self.exitcode)
+            self.assertEqual(self.results[self.label][0]['result'], self.result)
