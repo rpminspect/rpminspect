9bca5fda9664c4d0a965454355715bb25f9a9ff0
David Cantrell <dcantrell@redhat.com>
2019-09-25 14:29:09 -0400

    Use git archive when building on Copr
    No reason to run meson and then ninja dist.

--- .copr/installate-packages.sh
+++ .copr/installate-packages.sh
@@ -9,11 +9,7 @@ TOPDIR="$1"
 yum --help >/dev/null 2>&1
 [ $? -eq 0 ] && INSTALLATOR=yum || INSTALLATOR=dnf
 
-# Test dependencies not listed as BuildRequires
-TEST_PKGS="CUnit-devel valgrind gcovr python3-pip rpm-build"
-
 # The actual BuildRequires
 BUILD_REQUIRES="$(grep ^BuildRequires: ${TOPDIR}/rpminspect.spec.in | awk '{ print $2; }')"
 
-${INSTALLATOR} install -y git ${TEST_PKGS} ${BUILD_REQUIRES}
-pip-3 install rpmfluff
+${INSTALLATOR} install -y git ${BUILD_REQUIRES}
