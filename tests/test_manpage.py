cf78b60f80a2d00f5b5d0c0bd30c3ddc6f161efe
David Cantrell <dcantrell@redhat.com>
2019-10-17 15:30:33 -0400

    Stubbed out test_manpage.py test cases.

---
+++ tests/test_manpage.py
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2019  Red Hat, Inc.
+# Author(s):  David Cantrell <dcantrell@redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from baseclass import *
+
+# Man page in the correct section subdirectory in RPM (OK)
+#class TestManPageCorrectSectionRPM(TestRPMs):
+#XXX
+
+# Man page in the correct section subdirectory in Koji build (OK)
+#class TestManPageCorrectSectionKoji(TestKoji):
+#XXX
+
+# Man page not gzipped in RPM (VERIFY)
+#class TestManPageNotGzippedRPM(TestRPMs):
+#XXX
+
+# Man page not gzipped in Koji build (VERIFY)
+#class TestManPageNotGzippedKoji(TestKoji):
+#XXX
+
+# Man page in wrong section subdirectory in RPM (VERIFY)
+#class TestManPageWrongSectionRPM(TestRPMs):
+#XXX
+
+# Man page in wrong section subdirectory in Koji build (VERIFY)
+#class TestManPageWrongSectionKoji(TestKoji):
+#XXX
+
+# Invalid man page syntax in RPM (VERIFY)
+#class TestInvalidManPageRPM(TestRPMs):
+#XXX
+
+# Invalid man page syntax in Koji build (VERIFY)
+#class TestInvalidManPageKoji(TestKoji):
+#XXX
