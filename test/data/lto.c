ca4526815441807b337ddeb2a0a25f324df2f6b1
David Cantrell <dcantrell@redhat.com>
2020-05-05 13:40:33 -0400

    Add 'LTO' inspection test cases (#129)
    Check RPMs, Koji builds, and comparisons between both for the
    following:
    
        * .o and .a files present without LTO symbols (OK)
        * .o and .a files present with LTO symbols (BAD)
    
    All tests pass.

---
+++ test/data/lto.c
@@ -0,0 +1,19 @@
+double add(double x, double y)
+{
+    return x + y;
+}
+
+double subtract(double x, double y)
+{
+    return x - y;
+}
+
+double multiply(double x, double y)
+{
+    return x * y;
+}
+
+double divide(double x, double y)
+{
+    return x / y;
+}
