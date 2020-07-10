1e46e320d550f337ab1f6881dbac2ccce5871254
David Cantrell <dcantrell@redhat.com>
2020-07-10 13:21:55 -0400

    OK, just copy rpmfluff.py in place

--- osdeps/centos7/post.sh
+++ osdeps/centos7/post.sh
@@ -4,6 +4,5 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin
 # Install rpmfluff manually
 git clone https://pagure.io/rpmfluff.git
 cd rpmfluff
-sed -i -e '/^\#\!/d' setup.py
-python3 setup.py build
-python3 setup.py install
+SITE_PACKAGES="$(python3 -c "import sysconfig ; print(sysconfig.get_paths()['purelib'])")"
+install -D -m 0644 rpmfluff/rpmfluff.py ${SITE_PACKAGES}/rpmfluff.py
