2eb1aa680dad93be05abc15b2f7caf12b42775fd
David Cantrell <dcantrell@redhat.com>
2020-07-10 13:29:18 -0400

    Make sure to manually install rpmfluff-0.5.7.1 for centos7
    The master branch has moved on and is not the same structure as the
    0.5.x line.

--- osdeps/centos7/post.sh
+++ osdeps/centos7/post.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
-# Install rpmfluff manually
+# Install rpmfluff-0.5.7.1 manually
+TAG=0.5.7.1
 git clone https://pagure.io/rpmfluff.git
 cd rpmfluff
+git checkout -b ${TAG} ${TAG}
 SITE_PACKAGES="$(python3 -c "import sysconfig ; print(sysconfig.get_paths()['purelib'])")"
-install -D -m 0644 rpmfluff/rpmfluff.py ${SITE_PACKAGES}/rpmfluff.py
+install -D -m 0644 rpmfluff.py ${SITE_PACKAGES}/rpmfluff.py
