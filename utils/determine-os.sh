24404a86f407a1ebbdc126dd1a8462c8a2ec414d
David Cantrell <dcantrell@redhat.com>
2020-07-09 20:17:44 -0400

    Small fixes to determine-os.sh

--- utils/determine-os.sh
+++ utils/determine-os.sh
@@ -8,26 +8,26 @@ PATH=/bin:/usr/bin
 CWD="$(pwd)"
 VERSION_ID=0
 
-if [ -f /etc/os-release ]; then
+if [ -r /etc/os-release ]; then
     . /etc/os-release
 fi
 
-if [ -f /etc/fedora-release ] && [ "${ID}" = "fedora" ]; then
+if [ -r /etc/fedora-release ] && [ "${ID}" = "fedora" ]; then
     echo "${ID}"
-elif [ -f /etc/centos-release ] && [ "${ID}" = "centos" ]; then
+elif [ -r /etc/centos-release ] && [ "${ID}" = "centos" ]; then
     if [ ${VERSION_ID} -eq 7 ] || [ ${VERSION_ID} -eq 8 ]; then
         echo "${ID}${VERSION_ID}"
     else
         echo "unknown"
     fi
-elif [ -f /etc/redhat-release ] && [ "${ID}" = "rhel" ]; then
+elif [ -r /etc/redhat-release ] && [ "${ID}" = "rhel" ]; then
     v="$(echo "${VERSION_ID}" | cut -d '.' -f 1)"
     if [ "${v}" = "7" ] || [ "${v}" = "8" ]; then
         echo "${ID}${v}"
     else
         echo "unknown"
     fi
-elif [ -f /etc/SuSE-release ] && [ "${ID}" = "opensuse-leap" ]; then
+elif [ -r /etc/SuSE-release ] && [ "${ID}" = "opensuse-leap" ]; then
     echo "${ID}"
 elif [ "${ID}" = "ubuntu" ]; then
     echo "${ID}"
