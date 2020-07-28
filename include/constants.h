838a4d3fc8367907cc9765a425e9e06dd5e3867f
David Cantrell <dcantrell@redhat.com>
2020-07-28 11:49:37 -0400

    Relicense librpminspect (lib/ and include/) as LGPL-3.0-or-later
    This commit changes the license on librpminspect from GPL-3.0-or-later
    to LGPL-3.0-or-later.  The rpminspect program (src/) is still
    GPL-3.0-or-later.
    
    There are 5 source files in lib/ that carry Apache 2.0 licenses
    (checksums.c, copyfile.c, mkdirp.c, rmtree.c, and unpack.c), but these
    are ok with GPL and LGPL 3.0 or later projects.  The entirety of
    librpminspect is still under LGPL-3.0-or-later.

--- include/constants.h
+++ include/constants.h
@@ -2,18 +2,21 @@
  * Copyright (C) 2018-2020  Red Hat, Inc.
  * Author(s):  David Cantrell <dcantrell@redhat.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
 /**
@@ -21,7 +24,7 @@
  * @author David Cantrell &lt;dcantrell@redhat.com&gt;
  * @date 2018-2020
  * @brief Constants and other defaults for librpminspect.
- * @copyright GPL-3.0-or-later
+ * @copyright LGPL-3.0-or-later
  */
 
 #ifndef _LIBRPMINSPECT_CONSTANTS_H
--- lib/ignore.c
+++ lib/ignore.c
@@ -2,18 +2,21 @@
  * Copyright (C) 2020  Red Hat, Inc.
  * Red Hat Author(s):  David Cantrell <dcantrell@redhat.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
 #include <stdlib.h>
@@ -31,7 +34,7 @@
  * @author David Cantrell &lt;dcantrell@redhat.com&gt;
  * @date 2020
  * @brief Functions for handling the 'ignores' from the config file.
- * @copyright GPL-3.0-or-later
+ * @copyright LGPL-3.0-or-later
  */
 
 /**
