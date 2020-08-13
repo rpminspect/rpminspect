72b04300d292966366adf91e3453a44a851ae7dc
David Cantrell <dcantrell@redhat.com>
2020-08-13 12:03:47 -0400

    libgcc-s1:i386 -> lib32gcc-s1 for debian CI

--- osdeps/debian/pre.sh
+++ osdeps/debian/pre.sh
@@ -2,5 +2,5 @@
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 dpkg --add-architecture i386
 apt-get -y install libterm-readline-perl-perl
-apt-get -y install libgcc-s1:i386
+apt-get -y install lib32gcc-s1
 apt-get update
