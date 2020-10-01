1c2389fb6b13ccb2bb460fbaaba4485a91480207
David Cantrell <dcantrell@redhat.com>
2020-10-01 16:47:26 -0400

    Python formatting fixes for test_virus.py

--- test/test_virus.py
+++ test/test_virus.py
@@ -19,75 +19,100 @@
 from baseclass import TestSRPM, TestRPMs, TestKoji
 from baseclass import TestCompareSRPM, TestCompareRPMs, TestCompareKoji
 
+
 # package that does not contain a virus (OK)
 class HasNoVirusSRPM(TestSRPM):
     def setUp(self):
         super().setUp()
 
-        self.rpm.add_simple_library(libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj")
+        self.rpm.add_simple_library(
+            libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj"
+        )
 
         self.inspection = "virus"
         self.label = "virus"
         self.result = "OK"
         self.waiver_auth = "Not Waivable"
+
 
 class HasNoVirusRPMs(TestRPMs):
     def setUp(self):
         super().setUp()
 
-        self.rpm.add_simple_library(libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj")
+        self.rpm.add_simple_library(
+            libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj"
+        )
 
         self.inspection = "virus"
         self.label = "virus"
         self.result = "OK"
         self.waiver_auth = "Not Waivable"
+
 
 class HasNoVirusKoji(TestKoji):
     def setUp(self):
         super().setUp()
 
-        self.rpm.add_simple_library(libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj")
+        self.rpm.add_simple_library(
+            libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj"
+        )
 
         self.inspection = "virus"
         self.label = "virus"
         self.result = "OK"
         self.waiver_auth = "Not Waivable"
+
 
 class HasNoVirusCompareSRPM(TestCompareSRPM):
     def setUp(self):
         super().setUp()
 
-        self.before_rpm.add_simple_library(libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj")
-        self.after_rpm.add_simple_library(libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj")
+        self.before_rpm.add_simple_library(
+            libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj"
+        )
+        self.after_rpm.add_simple_library(
+            libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj"
+        )
 
         self.inspection = "virus"
         self.label = "virus"
         self.result = "OK"
         self.waiver_auth = "Not Waivable"
+
 
 class HasNoVirusCompareRPMs(TestCompareRPMs):
     def setUp(self):
         super().setUp()
 
-        self.before_rpm.add_simple_library(libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj")
-        self.after_rpm.add_simple_library(libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj")
+        self.before_rpm.add_simple_library(
+            libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj"
+        )
+        self.after_rpm.add_simple_library(
+            libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj"
+        )
 
         self.inspection = "virus"
         self.label = "virus"
         self.result = "OK"
         self.waiver_auth = "Not Waivable"
+
 
 class HasNoVirusCompareKoji(TestCompareKoji):
     def setUp(self):
         super().setUp()
 
-        self.before_rpm.add_simple_library(libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj")
-        self.after_rpm.add_simple_library(libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj")
+        self.before_rpm.add_simple_library(
+            libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj"
+        )
+        self.after_rpm.add_simple_library(
+            libraryName="whte_rbt.obj", installPath="usr/lib/whte_rbt.obj"
+        )
 
         self.inspection = "virus"
         self.label = "virus"
         self.result = "OK"
         self.waiver_auth = "Not Waivable"
+
 
 # package that contains a virus (BAD)
 class HasVirusSRPM(TestSRPM):
@@ -101,6 +126,7 @@ class HasVirusSRPM(TestSRPM):
         self.result = "BAD"
         self.waiver_auth = "Anyone"
 
+
 class HasVirusRPMs(TestRPMs):
     def setUp(self):
         super().setUp()
@@ -111,6 +137,7 @@ class HasVirusRPMs(TestRPMs):
         self.label = "virus"
         self.result = "BAD"
         self.waiver_auth = "Anyone"
+
 
 class HasVirusKoji(TestKoji):
     def setUp(self):
@@ -123,36 +150,51 @@ class HasVirusKoji(TestKoji):
         self.result = "BAD"
         self.waiver_auth = "Anyone"
 
+
 class HasVirusCompareSRPM(TestCompareSRPM):
     def setUp(self):
         super().setUp()
 
-        self.before_rpm.add_fake_virus("usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755")
-        self.after_rpm.add_fake_virus("usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755")
+        self.before_rpm.add_fake_virus(
+            "usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755"
+        )
+        self.after_rpm.add_fake_virus(
+            "usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755"
+        )
 
         self.inspection = "virus"
         self.label = "virus"
         self.result = "BAD"
         self.waiver_auth = "Anyone"
+
 
 class HasVirusCompareRPMs(TestCompareRPMs):
     def setUp(self):
         super().setUp()
 
-        self.before_rpm.add_fake_virus("usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755")
-        self.after_rpm.add_fake_virus("usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755")
+        self.before_rpm.add_fake_virus(
+            "usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755"
+        )
+        self.after_rpm.add_fake_virus(
+            "usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755"
+        )
 
         self.inspection = "virus"
         self.label = "virus"
         self.result = "BAD"
         self.waiver_auth = "Anyone"
 
+
 class HasVirusCompareKoji(TestCompareKoji):
     def setUp(self):
         super().setUp()
 
-        self.before_rpm.add_fake_virus("usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755")
-        self.after_rpm.add_fake_virus("usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755")
+        self.before_rpm.add_fake_virus(
+            "usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755"
+        )
+        self.after_rpm.add_fake_virus(
+            "usr/lib/whte_rbt.obj", "whte_rbt.pas", mode="0755"
+        )
 
         self.inspection = "virus"
         self.label = "virus"
