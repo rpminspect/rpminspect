d8a3d74d796fbd691b986f914affc09d88e7b757
David Cantrell <dcantrell@redhat.com>
2021-02-15 16:04:42 -0500

    [tst] Pass -D to rpminspect in the test suite
    On failure, make sure it dumps the config file structure used for the
    test case.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- test/baseclass.py
+++ test/baseclass.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2020  Red Hat, Inc.
+# Copyright (C) 2019-2021  Red Hat, Inc.
 # Author(s):  David Cantrell <dcantrell@redhat.com>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -209,6 +209,7 @@ class TestSRPM(RequiresRpminspect):
 
         args = [
             self.rpminspect,
+            "-D",
             "-d",
             "-c",
             self.conffile,
