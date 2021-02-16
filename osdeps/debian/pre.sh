5dfb00bd3e3dc5293b25145779aa5cd4fd96480f
David Cantrell <dcantrell@redhat.com>
2021-02-16 13:31:47 -0500

    [ci] Update pip and setuptools on debian and ubuntu CI jobs
    Make sure the latest pip and setuptools are available.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/debian/pre.sh
+++ osdeps/debian/pre.sh
@@ -4,3 +4,7 @@ dpkg --add-architecture i386
 apt-get -y install libterm-readline-perl-perl
 apt-get -y install lib32gcc-s1
 apt-get update
+
+# install and update pip and setuptools
+apt-get install python3-pip python3-setuptools
+pip3 install --upgrade pip setuptools
