77ef2ce8a0d4f2e614d556fa45520c74a89522ff
David Cantrell <dcantrell@redhat.com>
2021-02-16 14:26:31 -0500

    [ci] Fixes for extra-ci on arch, centos7, centos8, and debian
    Arch Linux needs the initialization process to create the
    /var/lib/pacman directory.  centos7 and centos8 had the freshclam call
    in pre.sh, which was wrong.  It runs from post.sh.  And debian needed
    the '-y' switch to apt-get in pre.sh when installing pip.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/arch/pre.sh
+++ osdeps/arch/pre.sh
@@ -2,5 +2,5 @@
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin
 
 # Update pip and setuptools
-yum install -y python36-pip
+pacman --noconfirm -S python-pip
 pip3 install --upgrade pip setuptools
--- osdeps/centos8/pre.sh
+++ osdeps/centos8/pre.sh
@@ -4,6 +4,3 @@ PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin
 # Update pip and setuptools
 dnf install -y python3-pip
 pip3 install --upgrade pip setuptools
-
-# Update clamav database
-freshclam
