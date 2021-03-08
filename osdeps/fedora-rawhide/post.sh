6b805cbf03a2a0349fc09834bb8bc5f401b3f9d0
David Cantrell <dcantrell@redhat.com>
2021-03-08 17:25:54 -0500

    [ci] Adjust lib/meson.build for Fedora rawhide
    Syntax needed to change a bit for meson on rawhide.  Update the
    osdeps/fedora-rawhide/ files too and re-enable the GitHub Actions CI
    target for fedora-rawhide.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/fedora-rawhide/post.sh
+++ osdeps/fedora-rawhide/post.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
-[ -d "${OSDEPS}" ] || exit 1
-
-# Work around a bug in meson 0.55.0
-MESON_VER="$(meson --version 2>/dev/null)"
-
-if [ -z "${MESON_VER}" ] || [ ! "${MESON_VER}" = "0.55.0" ]; then
-    exit 0
-fi
-
-PYDIR="$(dirname $(rpm -ql meson | grep 'ninjabackend.py$'))"
-[ -d "${PYDIR}" ] || exit 2
-
-cd "${PYDIR}"
-[ -f "${OSDEPS}/meson.patch" ] || exit 3
-patch -p0 < "${OSDEPS}/meson.patch"
 
 # Update the clamav database
 freshclam
