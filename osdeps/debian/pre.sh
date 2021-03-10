8e5c68d68026d1d55cc51cb5f2fa1b869f2aa48d
David Cantrell <dcantrell@redhat.com>
2021-03-10 17:53:58 -0500

    [ci] Debian and Ubuntu fixes for CI
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/debian/pre.sh
+++ osdeps/debian/pre.sh
@@ -3,12 +3,12 @@ PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin
 
 if [ "$(uname -a)" = "x86_64" ]; then
     dpkg --add-architecture i386
+    apt-get -y install lib32gcc-s1
 fi
 
 apt-get -y install libterm-readline-perl-perl
-apt-get -y install lib32gcc-s1
 apt-get update
 
 # install and update pip and setuptools
 apt-get -y install python3-pip python3-setuptools
-pip3 install --upgrade pip setuptools
+pip-3 install --upgrade pip setuptools
--- osdeps/ubuntu/pre.sh
+++ osdeps/ubuntu/pre.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
-if [ "$(uname -m)" = "x86_64" ]; then
+if [ "$(uname -a)" = "x86_64" ]; then
     dpkg --add-architecture i386
+    apt-get -y install lib32gcc-s1
 fi
 
+apt-get -y install libterm-readline-perl-perl
 apt-get update
 
 # install and update pip and setuptools
 apt-get install python3-pip python3-setuptools
-pip3 install --upgrade pip setuptools
+pip-3 install --upgrade pip setuptools
