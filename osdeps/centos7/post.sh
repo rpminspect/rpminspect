38c7ed94e3190b9beea8d56341cc9877f7ba8e72
David Cantrell <dcantrell@redhat.com>
2021-03-10 13:02:05 -0500

    [ci] Split 32-bit osdeps out to post.sh scripts in osdeps/
    The 32-bit checks only really apply to x86_64 systems now when
    building for i686 or i386.  Drop those dependencies from the reqs.txt
    files and install them from the post.sh for the systems.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/centos7/post.sh
+++ osdeps/centos7/post.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
+# Install 32-bit development files on x86_64 systems
+if [ "$(uname -m)" = "x86_64" ]; then
+    yum install -y glibc-devel.i686 glibc.i686 libgcc.i686
+fi
+
 # Install rpmfluff-0.5.7.1 manually
 TAG=0.5.7.1
 git clone https://pagure.io/rpmfluff.git
