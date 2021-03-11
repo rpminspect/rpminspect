0386ba6268e7d10d27551a7727da4ee677fb48b0
David Cantrell <dcantrell@redhat.com>
2021-03-11 12:51:18 -0500

    [ci] Install s390 glibc headers on s390x fedora systems
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/fedora-rawhide/post.sh
+++ osdeps/fedora-rawhide/post.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
-# Install 32-bit development files on x86_64 systems
-if [ "$(uname -m)" = "x86_64" ]; then
-    dnf install -y glibc-devel.i686 glibc.i686 libgcc.i686 glibc-headers-x86
-fi
+# Install 32-bit development files on 64-bit systems when available
+case "$(uname -m)" in
+    x86_64)
+        dnf install -y glibc-devel.i686 glibc.i686 libgcc.i686 glibc-headers-x86
+        ;;
+    s390x)
+        dnf install -y glibc-headers-s390
+        ;;
+esac
 
 # Update the clamav database
 freshclam
--- osdeps/fedora/post.sh
+++ osdeps/fedora/post.sh
@@ -2,10 +2,15 @@
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 [ -d "${OSDEPS}" ] || exit 1
 
-# Install 32-bit development files on x86_64 systems
-if [ "$(uname -m)" = "x86_64" ]; then
-    dnf install -y glibc-devel.i686 glibc.i686 libgcc.i686 glibc-headers-x86
-fi
+# Install 32-bit development files on 64-bit systems when available
+case "$(uname -m)" in
+    x86_64)
+        dnf install -y glibc-devel.i686 glibc.i686 libgcc.i686 glibc-headers-x86
+        ;;
+    s390x)
+        dnf install -y glibc-headers-s390
+        ;;
+esac
 
 # Work around a bug in meson 0.55.0
 MESON_VER="$(meson --version 2>/dev/null)"
