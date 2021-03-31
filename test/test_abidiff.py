393022ad290bdbb43f52b338c06962241c51e07b
David Cantrell <dcantrell@redhat.com>
2021-03-31 10:52:41 -0400

    [tst] Make sure abidiff test cases add a DT_SONAME to the test lib
    Using rpmfluff, make sure the test libraries set a DT_SONAME since
    is_elf_shared_library() now requires that in order for the abidiff
    inspection to run on the file.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- test/test_abidiff.py
+++ test/test_abidiff.py
@@ -142,10 +142,10 @@ class AbidiffNoRebaseWithABIChangeRPMs(TestCompareRPMs):
         super().setUp()
 
         self.before_rpm.add_simple_library(
-            sourceContent=old_library_source, compileFlags="-g"
+            sourceContent=old_library_source, compileFlags="-g -Wl,-soname,libcrashy.so.1"
         )
         self.after_rpm.add_simple_library(
-            sourceContent=new_library_source, compileFlags="-g"
+            sourceContent=new_library_source, compileFlags="-g -Wl,-soname,libcrashy.so.1"
         )
 
         self.inspection = "abidiff"
@@ -159,10 +159,10 @@ class AbidiffNoRebaseWithABIChangeKoji(TestCompareKoji):
         super().setUp()
 
         self.before_rpm.add_simple_library(
-            sourceContent=old_library_source, compileFlags="-g"
+            sourceContent=old_library_source, compileFlags="-g -Wl,-soname,libcrashy.so.1"
         )
         self.after_rpm.add_simple_library(
-            sourceContent=new_library_source, compileFlags="-g"
+            sourceContent=new_library_source, compileFlags="-g -Wl,-soname,libcrashy.so.1"
         )
 
         self.inspection = "abidiff"
