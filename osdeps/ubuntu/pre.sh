c7f23c9a9d56374f9a01957daf584363c1028263
David Cantrell <dcantrell@redhat.com>
2021-03-31 15:22:40 -0400

    [ci] Fix and enable the Ubuntu extra-ci job in GitHub Actions
    Required updating the osdeps files to set up the system correctly and
    install all require build and runtime deps.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/ubuntu/pre.sh
+++ osdeps/ubuntu/pre.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
-if [ "$(uname -a)" = "x86_64" ]; then
+if [ "$(uname -m)" = "x86_64" ]; then
     dpkg --add-architecture i386
     apt-get -y install lib32gcc-s1
 fi
 
 apt-get -y install libterm-readline-perl-perl
-apt-get update
+apt-get -y update
 
 # install and update pip and setuptools
-apt-get install python3-pip python3-setuptools
-pip-3 install --upgrade pip setuptools
+apt-get install python3-pip python3-setuptools python3-testresources
+pip3 install --upgrade pip setuptools testresources
