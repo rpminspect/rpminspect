2768c80dc3eed058f7f2896cf1c82ad66aa41995
David Cantrell <dcantrell@redhat.com>
2021-04-15 16:18:24 -0400

    [ci] Install required Python modules in pre.sh on Arch Linux
    I think what's happening is pip is getting updated and then the latest
    pip fails to install the other modules.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/arch/pre.sh
+++ osdeps/arch/pre.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin
 
-# Update pip and setuptools
+# Install required Python modules
 pacman --noconfirm -S python-pip
-pip3 install --upgrade pip setuptools cpp-coveralls
+pip3 install --upgrade pip setuptools cpp-coveralls rpmfluff PyYAML gcovr timeout-decorator
