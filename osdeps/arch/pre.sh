153199b4fba56645c36a4015ac4e78e7dd11161d
David Cantrell <dcantrell@redhat.com>
2021-04-15 16:15:04 -0400

    [ci] Install cpp-coveralls in pre.sh on Arch Linux
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/arch/pre.sh
+++ osdeps/arch/pre.sh
@@ -3,4 +3,4 @@ PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin
 
 # Update pip and setuptools
 pacman --noconfirm -S python-pip
-pip3 install --upgrade pip setuptools
+pip3 install --upgrade pip setuptools cpp-coveralls
