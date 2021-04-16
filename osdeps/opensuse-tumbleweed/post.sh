ce7e882a0d7579a40ec2dc607b798f311125211d
David Cantrell <dcantrell@redhat.com>
2021-04-16 16:19:20 -0400

    [ci] Update the OpenSUSE Tumbleweed files, but disable it anyway
    Another problem on tumbleweed where the required packages can't even
    install without zypper giving an error like:
    
    Installation of dbus-1-1.12.20-5.1.x86_64 failed:
    Error: Subprocess failed. Error: RPM failed: ERROR: neither useradd nor busybox found!
    error: %prein(dbus-1-1.12.20-5.1.x86_64) scriptlet failed, exit status 1
    error: dbus-1-1.12.20-5.1.x86_64: install failed
    
    Abort, retry, ignore? [a/r/i] (a): a
    Problem occurred during or after installation or removal of packages:
    Installation has been aborted as directed.
    Please see the above error message for a hint.
    
    I have seen this stuff before and it usually gets sorted out in a
    couple days, but to me tumbleweed is too unreliable to keep in the CI
    environment.  I will leave leap enabled to get some opensuse coverage.
    But for now I am back to turning off tumbleweed as a build target.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/opensuse-tumbleweed/post.sh
+++ osdeps/opensuse-tumbleweed/post.sh
@@ -5,7 +5,7 @@ CWD="$(pwd)"
 # The mandoc package on OpenSUSE lacks libmandoc.a and
 # header files, which we need to build rpminspect
 curl -O http://mandoc.bsd.lv/snapshots/mandoc.tar.gz
-SUBDIR="$(tar -tvf mandoc.tar.gz | head -n 1 | rev | cut -d ' ' -f 1 | rev)"
+SUBDIR="$(tar -tvf mandoc.tar.gz | head -n 1 | awk '{ print $6; }')"
 tar -xvf mandoc.tar.gz
 cd ${SUBDIR}
 echo 'PREFIX=/usr/local'                     > configure.local
@@ -35,24 +35,15 @@ make lib-install
 cd ${CWD}
 rm -rf mandoc.tar.gz ${SUBDIR}
 
-# Download 'rc' source from Debian and build it locally OpenSUSE Leap
-# lacks the rc shell as an installable package
-#
-# NOTE: The ARCHIVE= line depends on the output of html2text.  You
-# must ensure you are looking at the output on the target system.  The
-# default html2text output on Fedora is not the same as what you see
-# on OpenSUSE, for instance.
-RC_URL=http://ftp.debian.org/debian/pool/main/r/rc
-ARCHIVE="$(curl -s -L ${RC_URL} | html2text | grep "\.orig\.tar" | awk '{ print $3; }' | sort -u | tail -n 1)"
-curl -O -L ${RC_URL}/${ARCHIVE}
-SUBDIR="$(tar -tvf ${ARCHIVE} | head -n 1 | rev | cut -d ' ' -f 1 | rev)"
-tar -xvf ${ARCHIVE}
-cd ${SUBDIR}
+# The 'rc' shell is not available in Arch Linux, build manually
+git clone https://github.com/rakitzis/rc.git
+cd rc
+autoreconf -f -i -v
 ./configure --prefix=/usr/local
 make
 make install
 cd ${CWD}
-rm -rf ${ARCHIVE} ${SUBDIR}
+rm -rf rc
 
 # Update clamav database
 freshclam
