167f72c9b46cde584d40b52882aa012ef93a2b4d
David Cantrell <dcantrell@redhat.com>
2021-04-29 16:56:06 -0400

    [ci] Manually install mandoc on centos7 for now
    We need at least 1.14.5 to test against, so manually install that for
    now.  Eventually EPEL-7 will have the newer version and then this
    patch can be reverted.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/centos7/post.sh
+++ osdeps/centos7/post.sh
@@ -14,5 +14,32 @@ git checkout -b ${TAG} ${TAG}
 SITE_PACKAGES="$(python3 -c "import sysconfig ; print(sysconfig.get_paths()['purelib'])")"
 install -D -m 0644 rpmfluff.py ${SITE_PACKAGES}/rpmfluff.py
 
+# Install the latest mandoc package to /usr/local, the official EPEL-7
+# repos may be dated
+curl -O http://mandoc.bsd.lv/snapshots/mandoc.tar.gz
+SUBDIR="$(tar -tvf mandoc.tar.gz | head -n 1 | rev | cut -d ' ' -f 1 | rev)"
+tar -xvf mandoc.tar.gz
+cd ${SUBDIR}
+echo 'PREFIX=/usr/local'                     > configure.local
+echo 'BINDIR=/usr/local/bin'                >> configure.local
+echo 'SBINDIR=/usr/local/sbin'              >> configure.local
+echo 'MANDIR=/usr/local/share/man'          >> configure.local
+echo 'INCLUDEDIR=/usr/local/include'        >> configure.local
+echo 'LIBDIR=/usr/local/lib'                >> configure.local
+echo 'LN="ln -sf"'                          >> configure.local
+echo 'MANM_MANCONF=mandoc.conf'             >> configure.local
+echo 'INSTALL_PROGRAM="install -D -m 0755"' >> configure.local
+echo 'INSTALL_LIB="install -D -m 0644"'     >> configure.local
+echo 'INSTALL_HDR="install -D -m 0644"'     >> configure.local
+echo 'INSTALL_MAN="install -D -m 0644"'     >> configure.local
+echo 'INSTALL_DATA="install -D -m 0644"'    >> configure.local
+echo 'INSTALL_LIBMANDOC=1'                  >> configure.local
+echo 'CFLAGS="-g -fPIC"'                    >> configure.local
+./configure
+make
+make lib-install
+cd ..
+rm -rf mandoc.tar.gz ${SUBDIR}
+
 # Update clamav database
 freshclam
