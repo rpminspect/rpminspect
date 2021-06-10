4f484cd21badac19ed723348b8fb2c1b1e2f4d94
David Cantrell <dcantrell@redhat.com>
2021-06-10 14:26:36 -0400

    [ci] Restore previous centos7 pip and setuptools behavior
    Not sure why this was not working in my test environment earlier, but
    this is what I need to be doing so restore the behavior.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/centos7/pre.sh
+++ osdeps/centos7/pre.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin
 
-# EPEL is required
-yum install -y epel-release
-rpm --import /etc/pki/rpm-gpg/*
+# Update pip and setuptools
+dnf install -y python3-pip
+pip3 install --upgrade pip setuptools
