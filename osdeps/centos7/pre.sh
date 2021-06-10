10bd7834ef26b098f58df18f100c783881642717
David Cantrell <dcantrell@redhat.com>
2021-06-10 13:11:55 -0400

    [ci] Install epel-release in pre.sh on centos7
    Modify the osdeps scripts for centos7 a bit.  This makes it work more
    cleanly with 'make instreqs' in addition to GitHub Actions.  Install
    setuptools using the python36 package from EPEL.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/centos7/pre.sh
+++ osdeps/centos7/pre.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin
 
-# Update pip and setuptools
-yum install -y python36-pip
-pip3 install --upgrade pip setuptools
+# EPEL is required
+yum install -y epel-release
+rpm --import /etc/pki/rpm-gpg/*
