f824aeec6a09beda8dbfbc5a06413b3a1a44563a
David Cantrell <dcantrell@redhat.com>
2021-07-01 17:16:09 -0400

    [ci] Add Mageia Linux to Extra CI
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

---
+++ osdeps/mageia/post.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+CWD="$(pwd)"
+
+# Mageia Linux does not have mandoc
+curl -O http://mandoc.bsd.lv/snapshots/mandoc.tar.gz
+SUBDIR="$(tar -tvf mandoc.tar.gz | head -n 1 | rev | cut -d ' ' -f 1 | rev)"
+tar -xvf mandoc.tar.gz
+cd ${SUBDIR}
+echo 'PREFIX=/usr/local'                     > configure.local
+echo 'BINDIR=/usr/local/bin'                >> configure.local
+echo 'SBINDIR=/usr/local/sbin'              >> configure.local
+echo 'MANDIR=/usr/local/share/man'          >> configure.local
+echo 'INCLUDEDIR=/usr/local/include'        >> configure.local
+echo 'LIBDIR=/usr/local/lib'                >> configure.local
+echo 'LN="ln -sf"'                          >> configure.local
+echo 'MANM_MANCONF=mandoc.conf'             >> configure.local
+echo 'INSTALL_PROGRAM="install -D -m 0755"' >> configure.local
+echo 'INSTALL_LIB="install -D -m 0644"'     >> configure.local
+echo 'INSTALL_HDR="install -D -m 0644"'     >> configure.local
+echo 'INSTALL_MAN="install -D -m 0644"'     >> configure.local
+echo 'INSTALL_DATA="install -D -m 0644"'    >> configure.local
+echo 'INSTALL_LIBMANDOC=1'                  >> configure.local
+echo 'CFLAGS="-g -fPIC"'                    >> configure.local
+
+# something on Mageia causes the configure script grief, hack around it
+sed -i -e 's|^CC=.*$|CC=gcc|g' configure
+sed -i -e 's|^int dummy;$|extern int dummy;|g' compat_err.c
+sed -i -e 's|^int dummy;$|extern int dummy;|g' compat_getline.c
+sed -i -e 's|^int dummy;$|extern int dummy;|g' compat_reallocarray.c
+
+./configure
+make
+make lib-install
+cd ..
+rm -rf mandoc.tar.gz ${SUBDIR}
+
+# The 'rc' shell is not available in Mageia Linux, build manually
+git clone https://github.com/rakitzis/rc.git
+cd rc
+autoreconf -f -i -v
+./configure --prefix=/usr/local
+make
+make install
+
+# Install libabigail from git
+cd ${CWD}
+git clone https://sourceware.org/git/libabigail.git
+cd libabigail
+TAG="$(git tag -l | grep ^libabigail- | grep -v '\.rc' | sort -n | tail -n 1)"
+git checkout -b ${TAG} ${TAG}
+autoreconf -f -i -v
+./configure --prefix=/usr/local
+make
+make install
+
+# Update the clamav database
+freshclam
