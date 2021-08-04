2d01ccb8ac6bcfb7df8996157187e03237351992
David Cantrell <dcantrell@redhat.com>
2021-08-04 13:59:47 -0400

    Drop invalid free() call in free.c
    This is actually coming in a pending commit that handle security
    rules.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- lib/free.c
+++ lib/free.c
@@ -169,7 +169,6 @@ void free_rpminspect(struct rpminspect *ri) {
             sentry = TAILQ_FIRST(ri->security);
             TAILQ_REMOVE(ri->security, sentry, items);
 
-            free(sentry->path);
             free(sentry->pkg);
             free(sentry->ver);
             free(sentry->rel);
