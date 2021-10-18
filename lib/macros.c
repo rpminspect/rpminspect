2b77977ca4f9c4469ecd5a14904c740c998d0603
David Cantrell <dcantrell@redhat.com>
2021-10-18 11:19:16 -0400

    [lib] Add load_macros() function to macros.c
    This function comes from code that was originally in
    inspect_disttag.c.  The idea is a function that will load RPM macros
    from the files defined in the config file and mark the struct
    rpminspect as having run.  That way we can call this function once for
    the lifetime of the program and have all of the macros loaded.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- lib/macros.c
+++ lib/macros.c
@@ -23,8 +23,40 @@
 #include <regex.h>
 #include <assert.h>
 #include <err.h>
+#include <rpm/rpmfileutil.h>
+#include <rpm/rpmmacro.h>
 #include "queue.h"
 #include "rpminspect.h"
+
+/**
+ * Initialize macros for use by librpm calls.  This function should
+ * only be called once for the lifetime of the program run.  The
+ * caller needs to also call rpmFreeMacros() during cleanup as well.
+ * This function can be safely called multiple times as it is guarded
+ * by the macros_loaded boolean.
+ */
+void load_macros(struct rpminspect *ri)
+{
+    char *mf = NULL;
+    char *macropath = NULL;
+
+    assert(ri != NULL);
+
+    if (ri->macros_loaded) {
+        return;
+    }
+
+    if (ri->macrofiles) {
+        macropath = list_to_string(ri->macrofiles, ":");
+        mf = rpmGetPath(macropath, NULL);
+        rpmInitMacros(NULL, mf);
+        free(macropath);
+        free(mf);
+    }
+
+    ri->macros_loaded = true;
+    return;
+}
 
 /**
  * @brief Given an RPM spec file, read all of the macro definitions in
