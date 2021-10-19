c34242ec0359ad251273f3216ae94afab5344ae3
David Cantrell <dcantrell@redhat.com>
2021-10-19 14:46:48 -0400

    [lib] Introduce mime_type() function in magic.c
    This is like get_mime_type(), but instead of taking an
    rpmfile_entry_t, it just takes a string representing the file path.
    get_mime_type() has been updated to use this function.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- lib/magic.c
+++ lib/magic.c
@@ -28,17 +28,58 @@
 #include "rpminspect.h"
 
 /*
+ * Return the MIME type of the specified file by path.  The caller is
+ * responsible for freeing the returned string.
+ */
+char *mime_type(const char *path)
+{
+    char *type = NULL;
+    char *pos = NULL;
+    const char *tmp = NULL;
+    magic_t cookie;
+
+    if (path == NULL) {
+        return NULL;
+    }
+
+    cookie = magic_open(MAGIC_MIME | MAGIC_CHECK);
+
+    if (cookie == NULL) {
+        warnx(_("unable to initialize the magic library"));
+        return NULL;
+    }
+
+    if (magic_load(cookie, NULL) != 0) {
+        warnx(_("unable to load the magic database: %s"), magic_error(cookie));
+        magic_close(cookie);
+        return NULL;
+    }
+
+    if ((tmp = magic_file(cookie, path)) != NULL) {
+        type = strdup(tmp);
+
+        /*
+         * Trim any trailing metadata after the MIME type, such
+         * as 'charset=binary' and stuff like that.
+         */
+        if ((pos = index(type, ';')) != NULL) {
+            *pos = '\0';
+            type = realloc(type, strlen(type) + 1);
+        }
+    }
+
+    magic_close(cookie);
+    return type;
+}
+
+/*
  * Return the MIME type of the specified file.  The type is cached in the
  * rpmfile_entry_t.  If that is not NULL, this function returns that value.
  * Otherwise it gets the MIME type, caches it, and returns the value.
  * The caller should not free the pointer returned.
  */
-char *get_mime_type(rpmfile_entry_t *file) {
-    char *ret = NULL;
-    char *pos = NULL;
-    const char *tmp = NULL;
-    magic_t cookie;
-
+char *get_mime_type(rpmfile_entry_t *file)
+{
     assert(file != NULL);
 
     /* MIME type is cached, return it */
@@ -48,33 +89,8 @@ char *get_mime_type(rpmfile_entry_t *file) {
 
     /* Get and cache MIME type */
     assert(file->fullpath != NULL);
-    cookie = magic_open(MAGIC_MIME | MAGIC_CHECK);
+    file->type = mime_type(file->fullpath);
 
-    if (cookie == NULL) {
-        warnx(_("unable to initialize the magic library"));
-        return ret;
-    }
-
-    if (magic_load(cookie, NULL) != 0) {
-        warnx(_("unable to load the magic database: %s"), magic_error(cookie));
-        magic_close(cookie);
-        return ret;
-    }
-
-    if ((tmp = magic_file(cookie, file->fullpath)) != NULL) {
-        file->type = strdup(tmp);
-
-        /*
-         * Trim any trailing metadata after the MIME type, such
-         * as 'charset=binary' and stuff like that.
-         */
-        if ((pos = index(file->type, ';')) != NULL) {
-            *pos = '\0';
-            file->type = realloc(file->type, strlen(file->type) + 1);
-        }
-    }
-
-    magic_close(cookie);
     return file->type;
 }
 
