139576bcfc8fc369471662c578d574a7a2bb54cb
David Cantrell <dcantrell@redhat.com>
2021-10-26 15:34:20 -0400

    [tst] Begin test_unicode.py with unicode inspection tests
    Begin with some simple pass and fail tests with source in the SRPM
    directly.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

---
+++ test/test_unicode.py
@@ -0,0 +1,300 @@
+#
+# Copyright © 2021 Red Hat, Inc.
+# Author(s): David Cantrell <dcantrell@redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import os
+import rpmfluff
+
+from baseclass import (
+    TestSRPM,
+    TestRPMs,
+    TestKoji,
+    TestCompareSRPM,
+    TestCompareRPMs,
+    TestCompareKoji,
+)
+
+datadir = os.environ["RPMINSPECT_TEST_DATA_PATH"]
+good_src = open(os.path.join(datadir, "unicode", "good.c")).read()
+bad_src = open(os.path.join(datadir, "unicode", "bad.c")).read()
+status_src = open(os.path.join(datadir, "unicode", "status.sh")).read()
+makefile_src = open(os.path.join(datadir, "unicode", "Makefile")).read()
+
+
+# The following tests run the unicode inspection with a known good source
+# file in the SRPM.  All of the results for these should be 'OK'.
+class UnicodeGoodSourceSRPM(TestSRPM):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known good source file directly in the SRPM
+        self.rpm.add_source(rpmfluff.SourceFile("good.c", good_src))
+
+        self.inspection = "unicode"
+        self.result = "OK"
+
+
+class UnicodeGoodSourceRPMs(TestRPMs):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known good source file directly in the SRPM
+        self.rpm.add_source(rpmfluff.SourceFile("good.c", good_src))
+
+        self.inspection = "unicode"
+        self.result = "OK"
+
+
+class UnicodeGoodSourceKoji(TestKoji):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known good source file directly in the SRPM
+        self.rpm.add_source(rpmfluff.SourceFile("good.c", good_src))
+
+        self.inspection = "unicode"
+        self.result = "OK"
+
+
+class UnicodeGoodSourceCompareSRPM(TestCompareSRPM):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.before_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+        self.after_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known good source file directly in the SRPM
+        self.before_rpm.add_source(rpmfluff.SourceFile("good.c", good_src))
+        self.after_rpm.add_source(rpmfluff.SourceFile("good.c", good_src))
+
+        self.inspection = "unicode"
+        self.result = "OK"
+
+
+class UnicodeGoodSourceCompareRPMs(TestCompareRPMs):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.before_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+        self.after_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known good source file directly in the SRPM
+        self.before_rpm.add_source(rpmfluff.SourceFile("good.c", good_src))
+        self.after_rpm.add_source(rpmfluff.SourceFile("good.c", good_src))
+
+        self.inspection = "unicode"
+        self.result = "OK"
+
+
+class UnicodeGoodSourceCompareKoji(TestCompareKoji):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.before_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+        self.after_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known good source file directly in the SRPM
+        self.before_rpm.add_source(rpmfluff.SourceFile("good.c", good_src))
+        self.after_rpm.add_source(rpmfluff.SourceFile("good.c", good_src))
+
+        self.inspection = "unicode"
+        self.result = "OK"
+
+
+# The following tests run the unicode inspection with a known good source
+# file in a tarball in the SRPM.  All of the results for these should be 'OK'.
+# class UnicodeGoodSourceArchiveSRPM(TestSRPM):
+#    def setUp(self):
+#        super().setUp()
+#
+#        self.rpm.add_source(
+#            rpmfluff.GeneratedTarball("%s-%s.tar.gz" % (AFTER_NAME, AFTER_VER),
+#                                      "%s-%s" % (AFTER_NAME, AFTER_VER),
+#                                      [rpmfluff.SourceFile("Makefile", makefile_src),
+#                                       rpmfluff.SourceFile("good.c", good_src),
+#                                       rpmfluff.SourceFile("status.sh", status_src)])
+#        )
+#        self.rpm.section_prep = "%setup\n"
+#        self.rpm.section_build += "make\n"
+#        self.rpm.section_install += "make install DESTDIR=%{buildroot}\n"
+#        sub = self.rpm.get_subpackage(None)
+#        sub.section_files += "%attr(0755,root,root) /usr/bin/status\n"
+#
+#        self.inspection = "unicode"
+#        self.result = "OK"
+
+
+# class UnicodeGoodSourceArchiveRPMs(TestRPMs):
+# class UnicodeGoodSourceArchiveKoji(TestKoji):
+# class UnicodeGoodSourceArchiveCompareSRPM(TestCompareSRPM):
+# class UnicodeGoodSourceArchiveCompareRPMs(TestCompareRPMs):
+# class UnicodeGoodSourceArchiveCompareKoji(TestCompareKoji):
+
+
+# The following tests run the unicode inspection with a known bad source file
+# in the SRPM.  All of the results for these should be 'BAD' with a waiver
+# authorization of "Security".
+class UnicodeBadSourceSRPM(TestSRPM):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known bad source file directly in the SRPM
+        self.rpm.add_source(rpmfluff.SourceFile("bad.c", bad_src))
+
+        self.inspection = "unicode"
+        self.waiver_auth = "Security"
+        self.result = "BAD"
+
+
+class UnicodeBadSourceRPMs(TestRPMs):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known bad source file directly in the SRPM
+        self.rpm.add_source(rpmfluff.SourceFile("bad.c", bad_src))
+
+        self.inspection = "unicode"
+        self.waiver_auth = "Security"
+        self.result = "BAD"
+
+
+class UnicodeBadSourceKoji(TestKoji):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known bad source file directly in the SRPM
+        self.rpm.add_source(rpmfluff.SourceFile("bad.c", bad_src))
+
+        self.inspection = "unicode"
+        self.waiver_auth = "Security"
+        self.result = "BAD"
+
+
+class UnicodeBadSourceCompareSRPM(TestCompareSRPM):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.before_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+        self.after_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known bad source file directly in the SRPM
+        self.before_rpm.add_source(rpmfluff.SourceFile("bad.c", bad_src))
+        self.after_rpm.add_source(rpmfluff.SourceFile("bad.c", bad_src))
+
+        self.inspection = "unicode"
+        self.waiver_auth = "Security"
+        self.result = "BAD"
+
+
+class UnicodeBadSourceCompareRPMs(TestCompareRPMs):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.before_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+        self.after_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known bad source file directly in the SRPM
+        self.before_rpm.add_source(rpmfluff.SourceFile("bad.c", bad_src))
+        self.after_rpm.add_source(rpmfluff.SourceFile("bad.c", bad_src))
+
+        self.inspection = "unicode"
+        self.waiver_auth = "Security"
+        self.result = "BAD"
+
+
+class UnicodeBadSourceCompareKoji(TestCompareKoji):
+    def setUp(self):
+        super().setUp()
+
+        # this creates enough of a spec file to appease rpmbuild
+        self.before_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+        self.after_rpm.add_installed_file(
+            "/usr/bin/status", rpmfluff.SourceFile("status.sh", status_src), mode=755
+        )
+
+        # drop our known bad source file directly in the SRPM
+        self.before_rpm.add_source(rpmfluff.SourceFile("bad.c", bad_src))
+        self.after_rpm.add_source(rpmfluff.SourceFile("bad.c", bad_src))
+
+        self.inspection = "unicode"
+        self.waiver_auth = "Security"
+        self.result = "BAD"
+
+
+# class UnicodeBadSourceArchiveSRPM(TestSRPM):
+# class UnicodeBadSourceArchiveRPMs(TestRPMs):
+# class UnicodeBadSourceArchiveKoji(TestKoji):
+# class UnicodeBadSourceArchiveCompareSRPM(TestCompareSRPM):
+# class UnicodeBadSourceArchiveCompareRPMs(TestCompareRPMs):
+# class UnicodeBadSourceArchiveCompareKoji(TestCompareKoji):
