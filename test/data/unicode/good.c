3063fd3091ac03ba56305a3241683b0ba11c550d
David Cantrell <dcantrell@redhat.com>
2021-10-27 14:22:17 -0400

    [tst] Complete test_unicode.py and use different example code
    Use known bad example code in C and assembly within test_unicode.py.
    Finish the inspections and make sure we test rpminspect looking at
    code directly in SRPM files as well as the extracted and patched
    source trees that %prep sets up from the spec file.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- test/data/unicode/good.c
+++ test/data/unicode/good.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv)
-{
-    printf("Hello, world!\n");
-    return EXIT_SUCCESS;
-}
+  void hello ()
+  {
+    __builtin_printf ("Hello world\n");
+  }
+  int main ()
+  {
+    hello ();
+    return 0;
+  }
