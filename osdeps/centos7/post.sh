232c59ffd67fb4146c7e44906a661ae76d845c2a
David Cantrell <dcantrell@redhat.com>
2021-11-29 15:38:22 -0500

    [ci] Expand PATH in post.sh for centos7 Extra CI

--- osdeps/centos7/post.sh
+++ osdeps/centos7/post.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PATH=/bin:/usr/bin:/sbin:/usr/sbin
+PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin
 CWD="$(pwd)"
 
 # Install 32-bit development files on x86_64 systems
