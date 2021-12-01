f8903d3872baa1911506053376c9dbd66cfda1e5
David Cantrell <dcantrell@redhat.com>
2021-12-01 11:28:36 -0500

    [ci] Fix ALT Linux job in Extra CI
    The metadata inspection was failing with the Vendor name checks.  On
    ALT Linux, the /usr/lib/rpm directory carries a lot of 'macros' files
    which force the %vendor to always be "ALT Linux Team".  I find this a
    bit odd because it means even locally built packages will always
    contain that vendor name.
    
    For the ALT Linux job in Extra CI, I strike those lines from the RPM
    macros files.  The test suite also carries a specific test suite
    Vendor rather than inheriting the one from data/generic.yaml since
    that might change.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- test/baseclass.py
+++ test/baseclass.py
@@ -37,11 +37,8 @@ AFTER_VER = "0.1"
 AFTER_REBASE_VER = "47.0"
 AFTER_REL = "2"
 
-# Handle the package Vendor differently on ALT Linux
-if os.path.isfile("/etc/altlinux-release") or os.path.isfile("/etc/alt-release"):
-    VENDOR = "ALT Linux Team"
-else:
-    VENDOR = "rpminspect Test Vendor Ltd."
+# Test suite Vendor name
+VENDOR = "rpminspect Test Vendor Ltd."
 
 # Set this to True to keep rpminspect results (useful to debug the test
 # suite but will make a big mess)
