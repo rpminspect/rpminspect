3ff016805a1fa9763ffb461f7f35bee9d19fcad9
David Cantrell <dcantrell@redhat.com>
2022-03-23 10:37:59 -0400

    [doc] More Doxygen comment headers in include/
    The Doxygen effort for the library API is ongoing.  Slowly making
    progress here.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- include/output.h
+++ include/output.h
@@ -32,24 +32,28 @@
 
 /**
  * @def FORMAT_TEXT
+ *
  * Constant for the text output format (the default).
  */
 #define FORMAT_TEXT    0
 
 /**
  * @def FORMAT_JSON
+ *
  * Constant for the JSON output format.
  */
 #define FORMAT_JSON    1
 
 /**
  * @def FORMAT_XUNIT
+ *
  * Constant for the XUnit output format.
  */
 #define FORMAT_XUNIT   2
 
 /**
  * @def FORMAT_SUMMARY
+ *
  * Constant for the summary output format.
  */
 #define FORMAT_SUMMARY 3
