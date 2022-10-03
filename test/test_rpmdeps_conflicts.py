45e4fb3930ba30dbb60e6e9879af6d22b4b1d1f2
David Cantrell <dcantrell@redhat.com>
2022-10-03 14:51:18 -0400

    [tst] Update rpmdeps test cases for the multiple providers case
    Soft dependencies need to all change to OK as the result for the
    multiple providers test case based on the most recent change to
    librpminspect to only report a problem for multiple providers when
    there is an explicit named Requires in another subpackage.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- test/test_rpmdeps_conflicts.py
+++ test/test_rpmdeps_conflicts.py
@@ -996,7 +996,7 @@ class MissingExplicitConflictsCompareKoji(TestCompareKoji):
         self.waiver_auth = "Anyone"
 
 
-# Multiple providers of the same thing (VERIFY)
+# Multiple providers of the same thing (OK)
 class MultipleProvidersSRPM(TestSRPM):
     @unittest.skipUnless(have_elfdeps, "system lacks %s executable" % elfdeps)
     def setUp(self):
@@ -1139,8 +1139,8 @@ class MultipleProvidersKoji(TestKoji):
         self.rpm.add_conflicts("vaporware-libs = %{version}-%{release}")
 
         self.inspection = "rpmdeps"
-        self.result = "VERIFY"
-        self.waiver_auth = "Anyone"
+        self.result = "OK"
+        self.waiver_auth = "Not Waivable"
 
 
 class MultipleProvidersCompareSRPM(TestCompareSRPM):
@@ -1369,5 +1369,5 @@ class MultipleProvidersCompareKoji(TestCompareKoji):
         self.after_rpm.add_conflicts("vaporware-libs = %{version}-%{release}")
 
         self.inspection = "rpmdeps"
-        self.result = "VERIFY"
-        self.waiver_auth = "Anyone"
+        self.result = "OK"
+        self.waiver_auth = "Not Waivable"
