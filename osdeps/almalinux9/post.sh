e7a1041ed8ea870e443546fc8847f76e4738c35b
David Cantrell <dcantrell@redhat.com>
2022-10-07 14:07:05 -0400

    [ci] Add Alma Linux 9 job to GHA
    Add osdeps files for Alma Linux 9 and enable an Alma Linux 9 CI job
    via GitHub Actions.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

---
+++ osdeps/almalinux9/post.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+CWD="$(pwd)"
+
+# Install 32-bit development files on x86_64 systems
+if [ "$(uname -m)" = "x86_64" ]; then
+    yum install -y glibc-devel.i686 glibc.i686 libgcc.i686
+fi
+
+# Have to build and install dash manually because it does not exist in
+# EL9 as an RPM
+git clone https://git.kernel.org/pub/scm/utils/dash/dash.git
+cd dash || exit 1
+TAG="$(git tag -l | sort -V | tail -n 1)"
+git checkout -b ${TAG} ${TAG}
+./autogen.sh
+./configure --prefix=/usr/local
+make V=1
+make V=1 install
+cd "${CWD}" || exit 1
+
+# Update clamav database
+freshclam
