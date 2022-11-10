c6afeb568cd8ebb26eb11cd8701b5baf6a2c0e3b
David Cantrell <dcantrell@redhat.com>
2022-11-10 09:58:41 -0500

    Modernize and simplify the license boilerplates in the code
    After discussing open source licensing and source code license
    boilerplates with representatives from the Red Hat Legal team, they
    directed me to Red Hat's current source code recommendations regarding
    copyright and license notices.  A lot has changed over the years and
    the accepted recommendation now from the Red Hat Legal department (Red
    Hat is my employer and pays me to work on rpminspect) is to use a
    format like this:
    
        /*
         * Copyright The PROJECT Authors
         * SPDX-License-Identifier: SPDX_LICENSE_EXPRESSION
         */
    
    (adjust comments depending on the language)
    
    Alternatively, Red Hat will permit a "Copyright Red Hat" line, but the
    general recommendation is to state the "project authors" as a
    collective.
    
    Noticeable right away is the lack of a copyright year.  Also, the
    usual GPL, LGPL, MIT, or BSD license boilerplate text is missing.  The
    nice thing here is that projects using git or some other version
    control system retain all of that information in the commit logs, so
    you can research projects that way to find the actual individual
    authors (who hold the copyrights) and the year of first publication.
    It is not necessary per current copyright law and conventions to state
    this information in a Copyright statement.
    
    Also, the lack of a (C), (c), or Unicode copyright symbol.  The (C)
    and (c) were never accepted anyway and because they followed the word
    "Copyright", that's the word that actually had effect.  The Unicode
    copyright symbol can be used in place of "Copyright", but the general
    recommendation is to avoid that because of the limitations of various
    text editors in use today.
    
    SPDX is a project that has cataloged open source licenses, given then
    short identifiers, and documented acceptance criteria for what license
    text matches that license.  The project is active and new licenses are
    added quarterly.  Fedora Linux has moved to preferring these
    identifiers in RPM spec files over the legacy Fedora names.  Find
    yours at https://spdx.org/licenses/
    
    What about what follows the word "Copyright".  The PROJECT Authors is
    sort of vague.  Well, when you write code you own it.  Your version
    control system has recorded that.  The Copyright statement is more of
    a heads up to other readers that they are looking at copyrighted code
    and that multiple authors have had their hands in it.  This project
    carries an AUTHORS.md file that lists all of the contributors, but
    that's not really necessary.  It's just a nice to have.
    
    If you don't use "The PROJECT Authors" after "Copyright", why does Red
    Hat recommend just "Red Hat" and not something like "Red Hat, Inc." or
    "Red Hat GmbH" or "Red Hat Czech, s.r.o."?  It turns out that actually
    makes it harder for lawyers should ownership should there ever be
    legal questions.  It is known and understood that "Red Hat" in this
    context refers to all of the legal entities that make up Red Hat, so
    keep the notice simple.  Still, it's beter to name the project authors
    rather that Red Hat (or your employer).
    
    NOTE: There are some source files that come from other projects and
    have been imported in to rpminspect.  Those license and copyright
    comment blocks remain untouched.  That consists of some Apache-2.0
    licensed files in lib/, the LGPL-2.1-or-later licensed libxdiff/
    subdirectory, the BSD-1-Clause licensed include/uthash.h, and the
    BSD-3-Clause licensed include/compat/queue.h.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- utils/mkannounce.sh
+++ utils/mkannounce.sh
@@ -4,20 +4,8 @@
 # blog post.  Groups git log summaries by category and only includes
 # those log entries that have a category marking.
 #
-# Copyright © 2021 David Cantrell <david.l.cantrell@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Copyright David Cantrell
+# SPDX-License-Identifier: GPL-3.0-or-later
 #
 
 PATH=/bin:/usr/bin
