fbf1dee664569afb6cc1459931b251ad70a99770
David Cantrell <dcantrell@redhat.com>
2023-02-01 14:53:40 -0500

    [ci] Install cdson from git on Oracle Linux
    Oracle Linux doesn't use EPEL from us.  Instead Oracle has this copy
    of EPEL, sort of.  I remember running in to this when I initially
    added the Oracle Linux CI job.  Just install cdson from git and
    continue.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/oraclelinux8/post.sh
+++ osdeps/oraclelinux8/post.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
+CWD="$(pwd)"
 
 # Install 32-bit development files on x86_64 systems
 if [ "$(uname -m)" = "x86_64" ]; then
     dnf install -y glibc-devel.i686 glibc.i686 libgcc.i686
 fi
+
+# cdson is not [yet] in Oracle Linux
+git clone https://github.com/frozencemetery/cdson.git
+cd cdson || exit 1
+TAG="$(git tag -l | sort -n | tail -n 1)"
+git checkout -b "${TAG}" "${TAG}"
+meson setup build
+ninja -C build -v
+meson -C build test
+meson -C build install
+cd "${CWD}" || exit 1
+rm -rf cdson
 
 # Update clamav database
 freshclam
