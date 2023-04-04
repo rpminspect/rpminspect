ada19ec4143abfb4ab2b320e248a4928ca9fad5d
David Cantrell <dcantrell@redhat.com>
2023-04-04 12:07:47 -0400

    [tst] Add test cases for the new 'virus' security rule type
    Test cases for the various states of the virus security rule.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- test/test_virus.py
+++ test/test_virus.py
@@ -205,3 +205,159 @@ class HasVirusCompareKoji(TestCompareKoji):
 
     def runTest(self):
         super().runTest()
+
+
+class HasKnownVirusSkipRPMs(TestRPMs):
+    def setUp(self):
+        super().setUp()
+        self.rpm.add_fake_virus("usr/lib/wrskip.o", "wrskip.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "OK"
+
+
+class HasKnownVirusSkipCompareRPMs(TestCompareRPMs):
+    def setUp(self):
+        super().setUp()
+        self.before_rpm.add_fake_virus("usr/lib/wrskip.o", "wrskip.pas", mode="0755")
+        self.after_rpm.add_fake_virus("usr/lib/wrskip.o", "wrskip.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "OK"
+
+
+class HasKnownVirusSkipKoji(TestKoji):
+    def setUp(self):
+        super().setUp()
+        self.rpm.add_fake_virus("usr/lib/wrskip.o", "wrskip.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "OK"
+
+
+class HasKnownVirusSkipCompareKoji(TestCompareKoji):
+    def setUp(self):
+        super().setUp()
+        self.before_rpm.add_fake_virus("usr/lib/wrskip.o", "wrskip.pas", mode="0755")
+        self.after_rpm.add_fake_virus("usr/lib/wrskip.o", "wrskip.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "OK"
+
+
+class HasKnownVirusInformRPMs(TestRPMs):
+    def setUp(self):
+        super().setUp()
+        self.rpm.add_fake_virus("usr/lib/wrinform.o", "wrinform.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "INFO"
+        self.waiver_auth = "Not Waivable"
+
+
+class HasKnownVirusInformCompareRPMs(TestCompareRPMs):
+    def setUp(self):
+        super().setUp()
+        self.before_rpm.add_fake_virus(
+            "usr/lib/wrinform.o", "wrinform.pas", mode="0755"
+        )
+        self.after_rpm.add_fake_virus("usr/lib/wrinform.o", "wrinform.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "INFO"
+        self.waiver_auth = "Not Waivable"
+
+
+class HasKnownVirusInformKoji(TestKoji):
+    def setUp(self):
+        super().setUp()
+        self.rpm.add_fake_virus("usr/lib/wrinform.o", "wrinform.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "INFO"
+        self.waiver_auth = "Not Waivable"
+
+
+class HasKnownVirusInformCompareKoji(TestCompareKoji):
+    def setUp(self):
+        super().setUp()
+        self.before_rpm.add_fake_virus(
+            "usr/lib/wrinform.o", "wrinform.pas", mode="0755"
+        )
+        self.after_rpm.add_fake_virus("usr/lib/wrinform.o", "wrinform.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "INFO"
+        self.waiver_auth = "Not Waivable"
+
+
+class HasKnownVirusVerifyRPMs(TestRPMs):
+    def setUp(self):
+        super().setUp()
+        self.rpm.add_fake_virus("usr/lib/wrverify.o", "wrverify.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "VERIFY"
+        self.waiver_auth = "Security"
+
+
+class HasKnownVirusVerifyCompareRPMs(TestCompareRPMs):
+    def setUp(self):
+        super().setUp()
+        self.before_rpm.add_fake_virus(
+            "usr/lib/wrverify.o", "wrverify.pas", mode="0755"
+        )
+        self.after_rpm.add_fake_virus("usr/lib/wrverify.o", "wrverify.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "VERIFY"
+        self.waiver_auth = "Security"
+
+
+class HasKnownVirusVerifyKoji(TestKoji):
+    def setUp(self):
+        super().setUp()
+        self.rpm.add_fake_virus("usr/lib/wrverify.o", "wrverify.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "VERIFY"
+        self.waiver_auth = "Security"
+
+
+class HasKnownVirusVerifyCompareKoji(TestCompareKoji):
+    def setUp(self):
+        super().setUp()
+        self.before_rpm.add_fake_virus(
+            "usr/lib/wrverify.o", "wrverify.pas", mode="0755"
+        )
+        self.after_rpm.add_fake_virus("usr/lib/wrverify.o", "wrverify.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "VERIFY"
+        self.waiver_auth = "Security"
+
+
+class HasKnownVirusFailRPMs(TestRPMs):
+    def setUp(self):
+        super().setUp()
+        self.rpm.add_fake_virus("usr/lib/wrfail.o", "wrfail.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "BAD"
+        self.waiver_auth = "Security"
+
+
+class HasKnownVirusFailCompareRPMs(TestCompareRPMs):
+    def setUp(self):
+        super().setUp()
+        self.before_rpm.add_fake_virus("usr/lib/wrfail.o", "wrfail.pas", mode="0755")
+        self.after_rpm.add_fake_virus("usr/lib/wrfail.o", "wrfail.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "BAD"
+        self.waiver_auth = "Security"
+
+
+class HasKnownVirusFailKoji(TestKoji):
+    def setUp(self):
+        super().setUp()
+        self.rpm.add_fake_virus("usr/lib/wrfail.o", "wrfail.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "BAD"
+        self.waiver_auth = "Security"
+
+
+class HasKnownVirusFailCompareKoji(TestCompareKoji):
+    def setUp(self):
+        super().setUp()
+        self.before_rpm.add_fake_virus("usr/lib/wrfail.o", "wrfail.pas", mode="0755")
+        self.after_rpm.add_fake_virus("usr/lib/wrfail.o", "wrfail.pas", mode="0755")
+        self.inspection = "virus"
+        self.result = "BAD"
+        self.waiver_auth = "Security"
