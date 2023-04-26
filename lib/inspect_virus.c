rpminspect/rpminspect

commit 66b0953946bcae3ca1bd29401d2f3dcf024e24fe

[lib] Load *.cld files in addition to *.cvd files for virus scans
clamav databases are either *.cvd files or *.cld files so make sure to
load both if we find them.

Fixes: #1155

Signed-off-by: David Cantrell <dcantrell@redhat.com>

lib/inspect_virus.c
@@ -148,7 +148,7 @@ bool inspect_virus(struct rpminspect *ri)
     errno = 0;
 
     while ((de = readdir(d)) != NULL) {
-        if (!strcmp(de->d_name, ".") || !strcmp(de->d_name, "..") || !strsuffix(de->d_name, ".cvd")) {
+        if (!strcmp(de->d_name, ".") || !strcmp(de->d_name, "..") || !strsuffix(de->d_name, ".cvd") || !strsuffix(de->d_name, ".cld")) {
             continue;
         }
 

commit 8a55349558569fae3f479bdfebbde7866ef6c645

[fix] Load both *.cld and *.cvd virus database files, if they exist
This just fixes a logical error that was accidentally introduced
in 66b0953.

Signed-off-by: Michal Srb <michal@redhat.com>

lib/inspect_virus.c
@@ -148,7 +148,7 @@ bool inspect_virus(struct rpminspect *ri)
     errno = 0;
 
     while ((de = readdir(d)) != NULL) {
-        if (!strcmp(de->d_name, ".") || !strcmp(de->d_name, "..") || !strsuffix(de->d_name, ".cvd") || !strsuffix(de->d_name, ".cld")) {
+        if (!strcmp(de->d_name, ".") || !strcmp(de->d_name, "..") || (!strsuffix(de->d_name, ".cvd") && !strsuffix(de->d_name, ".cld"))) {
             continue;
         }
 