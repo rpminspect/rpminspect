3d151f96b6091e7549f683f37608d588b7979e87
David Cantrell <dcantrell@redhat.com>
2023-09-07 15:39:12 -0400

    [fix] Memory leak fix in run_cmd_vp()
    Move the find_cmd() call in to the child process so we don't leak in
    the parent.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- lib/runcmd.c
+++ lib/runcmd.c
@@ -180,9 +180,6 @@ char *run_cmd_vp(int *exitcode, const char *workdir, char **argv)
         return NULL;
     }
 
-    /* find the command */
-    cmd = find_cmd(argv[0]);
-
     /* run the command */
     proc = fork();
 
@@ -201,6 +198,9 @@ char *run_cmd_vp(int *exitcode, const char *workdir, char **argv)
 
         setlinebuf(stdout);
         setlinebuf(stderr);
+
+        /* find the command */
+        cmd = find_cmd(argv[0]);
 
         /* run the command */
         if (execvp(cmd, argv) == -1) {
