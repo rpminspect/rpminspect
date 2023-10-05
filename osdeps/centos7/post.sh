eb8beb6206979d7c9c4d6c747755d11f292daafb
David Cantrell <dcantrell@redhat.com>
2023-10-05 10:32:55 -0400

    [ci] Small fixes for the CentOS 7 job
    Needed some more build dependencies for installation of Python
    modules.  And the python3-rpm rebuild stopped working, so simplify
    that so we get it installed in the test environment against a more
    recent Python.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/centos7/post.sh
+++ osdeps/centos7/post.sh
@@ -29,10 +29,10 @@ rpm -Uvh "${SRPM}" 2>/dev/null
 rm -f "${SRPM}"
 cd ~/rpmbuild/SPECS || exit 1
 sed -i -e 's|^Name:.*$|Name: python3-rpm-rebuild|g' python3-rpm.spec
-sed -i -e '/^test \-f.*\.egg-info$/ s/test\ \-f/test -d/' python3-rpm.spec
 # shellcheck disable=SC2046
 yum install -y $(rpmspec -q --buildrequires python3-rpm.spec)
 rpmbuild -ba \
+         --nocheck \
          --define "__python3 /usr/local/bin/python${PYTHON_VER}" \
          --define "python3_version ${PYTHON_VER}" \
          --define "python3_sitearch /usr/local/lib/python${PYTHON_VER}/site-packages" \
