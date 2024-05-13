a9e5d69732879f549e836ce0ca8e56a2bce2f494
David Cantrell <dcantrell@redhat.com>
2024-05-13 11:19:06 -0400

    [ci] Fix Ubuntu GitHub Actions job
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/ubuntu/defs.mk
+++ osdeps/ubuntu/defs.mk
@@ -1,2 +1,2 @@
 PKG_CMD = env DEBIAN_FRONTEND=noninteractive apt-get -y install
-PIP_CMD = pip3 install -I
+PIP_CMD = pip3 install --break-system-packages -I
--- osdeps/ubuntu/pre.sh
+++ osdeps/ubuntu/pre.sh
@@ -3,12 +3,5 @@ PATH=/usr/bin
 
 if [ "$(uname -m)" = "x86_64" ]; then
     dpkg --add-architecture i386
-    apt-get -y install lib32gcc-s1
+    env DEBIAN_FRONTEND=noninteractive apt-get -y install lib32gcc-s1
 fi
-
-apt-get -y install libterm-readline-perl-perl
-apt-get -y update
-
-# install and update pip and setuptools
-apt-get -y install python3-pip python3-setuptools python3-testresources
-pip3 install --upgrade pip setuptools testresources
