f617cd564798ef298957cbd331fd64274125f60a
David Cantrell <dcantrell@redhat.com>
2024-05-13 15:12:52 -0400

    [tst] Fix the Debian testing and stable GitHub Actions CI jobs
    Needed to update the package lists and scripts a bit to run without
    error.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/debian-stable/pre.sh
+++ osdeps/debian-stable/pre.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
-PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
+PATH=/usr/bin
 
 if [ "$(uname -m)" = "x86_64" ]; then
     dpkg --add-architecture i386
     apt-get -y install lib32gcc-s1
 fi
-
-apt-get -y install libterm-readline-perl-perl
-apt-get update
-
-# install and update pip and setuptools
-apt-get -y install python3-pip python3-setuptools
