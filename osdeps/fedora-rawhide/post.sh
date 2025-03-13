92eb7b3c0ae3808ee015c54803561b733d6b51fb
David Cantrell <dcantrell@redhat.com>
2025-03-13 14:55:08 -0400

    [ci] On Fedora systems, make sure /var/lib/clamav is 0755
    It must be writable by either the clamav user or group.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- osdeps/fedora-rawhide/post.sh
+++ osdeps/fedora-rawhide/post.sh
@@ -26,4 +26,5 @@ fi
 sed -i -e '/^%%global\ __debug_package\ 1\\/d' /usr/lib/rpm/macros
 
 # Update the clamav database
+chmod 0755 /var/lib/clamav
 freshclam
--- osdeps/fedora/post.sh
+++ osdeps/fedora/post.sh
@@ -18,4 +18,5 @@ esac
 sed -i -e '/^%%global\ __debug_package\ 1\\/d' /usr/lib/rpm/macros
 
 # Update the clamav database
+chmod 0755 /var/lib/clamav
 freshclam
