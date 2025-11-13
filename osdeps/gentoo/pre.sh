df9b504eb6461d3eda4e00d3e0e88824f10f1a27
Dave Cantrell <dcantrell@redhat.com>
2025-11-13 14:39:57 -0500

    [ci] Try to reduce the amount of stuff built on Gentoo
    Signed-off-by: Dave Cantrell <dcantrell@redhat.com>

--- osdeps/gentoo/pre.sh
+++ osdeps/gentoo/pre.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Enable Python support in things like rpm
-echo 'USE="caps python lzma"' >> /etc/portage/make.conf
+echo 'USE="${USE} caps python lzma"' >> /etc/portage/make.conf
+
+# Disable things
+echo 'USE="${USE} -milter -experimental -clamapp -bashlogger -mem-scramble -pgo -plugins -abyss -debug -gnome -qt5 -qt6"' >> /etc/portage/make.conf
 
 # Allow installation of "masked" packages
 [ -d /etc/portage ] || mkdir -p /etc/portage
