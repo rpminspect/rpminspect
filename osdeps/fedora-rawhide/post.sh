bcfbbfffcb7fbc66beaf4b1aadacba70fe57c805
Dave Cantrell <dcantrell@redhat.com>
2026-01-22 09:43:40 -0500

    [ci] Install libatomic.i686 on Fedora stable and rawhide jobs
    This library has found its way to a separate package, so make sure it
    gets installed in the test environment.
    
    Signed-off-by: Dave Cantrell <dcantrell@redhat.com>

--- osdeps/fedora-rawhide/post.sh
+++ osdeps/fedora-rawhide/post.sh
@@ -4,10 +4,10 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin
 # Install 32-bit development files on 64-bit systems when available
 case "$(uname -m)" in
     x86_64)
-        dnf5 install -y glibc-devel.i686 glibc.i686 libgcc.i686 glibc-headers-x86
+        dnf install -y glibc-devel.i686 glibc.i686 libgcc.i686 libatomic.i686 glibc-headers-x86
         ;;
     s390x)
-        dnf5 install -y glibc-headers-s390
+        dnf install -y glibc-headers-s390
         ;;
 esac
 
