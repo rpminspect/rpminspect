73c40aca6cebe5e50e4e9e1b3e89d4f69c1b98e1
Dave Cantrell <dcantrell@redhat.com>
2026-01-28 11:11:47 -0500

    [ci] Fix up the Gentoo CI jobs so they don't fail
    The rc shell needs to build with -std=gnu99 on gcc.  And we need to
    tell Gentoo to install the clamav commands when the clamav package is
    "emerged".  Yes, I would like to USE the commands that come with the
    command line program, thank you.
    
    Signed-off-by: Dave Cantrell <dcantrell@redhat.com>

--- osdeps/gentoo/pre.sh
+++ osdeps/gentoo/pre.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
+
 # Enable Python support in things like rpm
 echo 'USE="${USE} caps python lzma"' >> /etc/portage/make.conf
+[ -d /etc/portage/package.use ] || mkdir -p /etc/portage/package.use
+echo 'app-antivirus/clamav clamapp' > /etc/portage/package.use/clamav
 
 # Disable things
 echo 'USE="${USE} -milter -experimental -clamapp -bashlogger -mem-scramble -pgo -plugins -abyss -debug -gnome -qt5 -qt6"' >> /etc/portage/make.conf
